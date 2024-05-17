4e0c9664b531faa564839262e05bab8f5d135343
Mae-Hyong <maehyong02@gmail.com>
2024-05-17 13:46:02 +0900

    chore : Mobile pay Modify

--- pay.js
+++ pay.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
@@ -25,7 +26,7 @@ let tid;
 
 // CORS 설정
 app.all('/*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
@@ -38,17 +39,17 @@ app.post('/kakaopay', async(req, res) => {
             method: "post",
             url: "/v1/payment/ready",
             params: {
-                cid: CID,
-                partner_order_id: PARTNER_ORDER_ID,
-                partner_user_id: PARTNER_USER_ID,
-                item_name: "test_item_name",
-                quantity: 1,
-                total_amount: 22000,
-                vat_amount: 0,
-                tax_free_amount: 0,
-                approval_url: `http://localhost:3000/success?partner_order_id=${PARTNER_ORDER_ID}&partner_user_id=${PARTNER_USER_ID}&cid=${CID}`,
-                fail_url: "http://localhost:3000/fail",
-                cancel_url: "http://localhost:3000/cancel",
+                cid: CID, // 가맹점 코드 -> 팝업스토어 ID
+                partner_order_id: PARTNER_ORDER_ID, // 가맹점 주문 ID
+                partner_user_id: PARTNER_USER_ID, // 가맹점 사용자 ID
+                item_name: "test_item_name", // 상품 명
+                quantity: 1,  // 수량
+                total_amount: 22000, // 결제 금액
+                vat_amount: 0, // 부가세
+                tax_free_amount: 0, // 비과세
+                approval_url: `http://localhost:3000/success?partner_order_id=${PARTNER_ORDER_ID}&partner_user_id=${PARTNER_USER_ID}&cid=${CID}`, // 결제 성공 시 리디렉션될 URL
+                fail_url: "http://localhost:3000/fail", // 결제 실패 시 리디렉션될 URL
+                cancel_url: "http://localhost:3000/cancel", // 결제 취소 시 리디렉션될 URL
             }
         });
 
@@ -59,7 +60,7 @@ app.post('/kakaopay', async(req, res) => {
         tid = response.data.tid;
 
         // 카카오페이 결제 URL 응답
-        res.send(response.data.next_redirect_pc_url);
+        res.send(response.data.next_redirect_mobile_url);
     } catch (error) {
         console.error("카카오페이 결제 요청 실패:", error.message);
         res.status(500).send("카카오페이 결제 요청 실패");
@@ -76,11 +77,11 @@ app.get('/success', async(req, res) => {
             method: "post",
             url: "/v1/payment/approve",
             params: {
-                cid: param.cid,
-                tid: tid,
-                partner_order_id: param.partner_order_id,
-                partner_user_id: param.partner_user_id,
-                pg_token: param.pg_token,
+                cid: param.cid, // 가맹점 코드
+                tid: tid, // 결제 고유 번호
+                partner_order_id: param.partner_order_id, // 가맹점 주문 ID
+                partner_user_id: param.partner_user_id, // 가맹점 사용자 ID
+                pg_token: param.pg_token, // 결제 승인 요청 토큰
             }
         });
 
